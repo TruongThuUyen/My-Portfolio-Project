108c4e808b1cf4a10249db131ff0dee23cae2cc6
May0105 <88717349+May0105@users.noreply.github.com>
2025-11-05 16:07:17 +0700

    Change background color for loading page: Black to color-surface-secondary and add animation slide-up-to-top. Update project list

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,7 +2,7 @@ import '@/styles/loading.css';
 
 export default function Loading() {
   return (
-    <div className='flex items-center justify-center w-full h-screen '>
+    <div className='loading-wrapper flex items-center justify-center w-full h-screen '>
       <h1
         className='relative text-4xl sm:text-7xl font-extrabold text-[rgba(40, 44, 52, 0.8)]
              m-0'
